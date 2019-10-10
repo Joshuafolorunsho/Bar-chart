13b74a7fdf14eaa13bbcb57308a42b38494946cf
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2019-10-10 22:51:48 +0100

    selecting all the necessary elements and caching them into memory

--- script/app.js
+++ script/app.js
@@ -1,1 +1,8 @@
 // Selecting element and caching into memory
+span1 = document.querySelectorAll('span')[0];
+span2 = document.querySelectorAll('span')[1];
+span3 = document.querySelectorAll('span')[2];
+span4 = document.querySelectorAll('span')[3];
+span5 = document.querySelectorAll('span')[4];
+span6 = document.querySelectorAll('span')[5];
+span7 = document.querySelectorAll('span')[6];
